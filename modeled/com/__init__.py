6197df9085138970a57d2a307a8bec8f10281203
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-10-05 11:22:37 +0200

    __init__.py file header

--- modeled/com/__init__.py
+++ modeled/com/__init__.py
@@ -1,3 +1,24 @@
+# python-modeled.com
+#
+# Copyright (C) 2014 Stefan Zimmermann <zimmermann.code@gmail.com>
+#
+# python-modeled.com is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-modeled.com is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with python-modeled.com.  If not, see <http://www.gnu.org/licenses/>.
+
+"""modeled.com
+
+.. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
+"""
 from six import with_metaclass
 
 __all__ = ['mCOM']
